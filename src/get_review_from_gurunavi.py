acc75c41a4b79b1a40b1d144b4c7be28e4e3ca1e
Keisuke Kiriyama <krkr.xm.rkr7x@gmail.com>
2017-07-02 15:03:50 +0900

    review_collecter

--- src/get_review_from_gurunavi.py
+++ src/get_review_from_gurunavi.py
@@ -13,16 +13,45 @@ class review_collecter_from_gurunavi(info_collecter_from_gurunavi):
         self.request_url = 'https://api.gnavi.co.jp/PhotoSearchAPI/20150630/'
         self.review_df = pd.DataFrame()
         self.query = [('format', format),('hit_per_page', 10)]
+        self.responce_item_keys = ['vote_id','photo_genre_id','photo_genre_name',
+                                   'photo_scene_id','photo_scene_name','nickname',
+                                   'shop_id','shop_name','shop_url','prefname',
+                                   'menu_id','menu_name','menu_finish_flag','areaname_l',
+                                   'areaname_m','areaname_s','image_url','comment',
+                                   'total_score','category','distance','latitude',
+                                   'longitude','umaso_count','update_date','messages']
+
+
+    def create_request_url(self, *request_parameters):
+        self.__init__()
+        self.request_parameters = request_parameters
+        self.query.append(('keyid',self.keyid))
+        self.query.append(('offset_page',1))
+        for request_parameter in request_parameters:
+            query_element = request_parameter.split('=')
+            self.query.append((query_element[0], query_element[1]))
+        self.request_url += '?{0}'.format(urllib.parse.urlencode(self.query))
+
+
+    def create_request_url_offset(self, offset):
+        self.request_url = re.sub('offset_page=[0-9]+', 'offset_page=' + str(offset), self.request_url)
 
 
     def send_request(self):
         try:
-            result = urllib.request.urlopen(self.request_url).read()
+            result = json.loads(urllib.request.urlopen(self.request_url).read().decode('utf-8'))
         except ValueError:
             print('APIアクセスに失敗しました')
             return
-        self.data = json.loads(result.decode('utf-8'))['response']
-        #print(json.dumps(self.data, ensure_ascii=False, indent=4, separators=(',', ': ')))
+        try:
+            self.data = result['response']
+            self.print_json(self.data)
+        except:
+            if 'error' in result['gnavi']:
+                print(result['gnavi']['error'])
+            else:
+                print('error')
+            return
         if self.check_result():
             print('データの取得に成功しました')
             print('total hit count : ', self.data['total_hit_count'])
@@ -34,10 +63,6 @@ class review_collecter_from_gurunavi(info_collecter_from_gurunavi):
                     self.send_request_offset()
 
 
-    def create_request_url_offset(self, offset):
-        self.request_url = re.sub('offset=[0-9]+', 'offset_page=' + str(offset), self.request_url)
-
-
     def send_request_offset(self):
         try:
             result = urllib.request.urlopen(self.request_url).read()
@@ -45,32 +70,42 @@ class review_collecter_from_gurunavi(info_collecter_from_gurunavi):
             print('APIアクセスに失敗しました')
             return
         self.data = json.loads(result.decode('utf-8'))['response']
-        #print(json.dumps(self.data, ensure_ascii=False, indent=4, separators=(',', ': ')))
+        self.print_json(self.data)
         if self.check_result():
             self.create_review_dataframe_offset()
 
 
     def create_review_dataframe(self):
-        temp_dict = self.create_temp_dict()
+        temp_dict = self.create_temp_dict_modify()
         self.review_df = pd.DataFrame(temp_dict)
 
 
     def create_review_dataframe_offset(self):
-        temp_dict = self.create_temp_dict()
-        temp_df = pd.DataFrame(temp_dict)
+        temp_dict = self.create_temp_dict_modify()
+        temp_df = pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in temp_dict.items()]))
         self.review_df = pd.concat([self.review_df, temp_df])
 
 
-    def create_temp_dict(self):
-        headers = self.data['0']['photo'].keys()
+    def create_temp_dict_modify(self):
         temp_dict = {}
         for rest_idx in range(self.data['hit_per_page']):
-            values = list(self.data[str(rest_idx)]['photo'].values())
-            for header_idx, header in enumerate(headers):
-                if rest_idx == 0:
-                    temp_dict[header] = [values[header_idx]]
+            filled_key = []
+            for review_key in self.data[str(rest_idx)]['photo']:
+                if review_key in temp_dict:
+                    temp_dict[review_key].append(self.data[str(rest_idx)]['photo'][review_key])
+                    filled_key.append(review_key)
                 else:
-                    temp_dict[header].append(values[header_idx])
+                    temp_dict[review_key] = [self.data[str(rest_idx)]['photo'][review_key]]
+                    filled_key.append(review_key)
+            diff_item = self.compare_list(self.responce_item_keys, filled_key)
+            if len(diff_item) != 0:
+                for nan_item in diff_item:
+                    if nan_item in temp_dict:
+                        temp_dict[nan_item].append('')
+                    else:
+                        temp_dict[nan_item] = ['']
+
+
         return temp_dict
 
 
@@ -78,5 +113,19 @@ class review_collecter_from_gurunavi(info_collecter_from_gurunavi):
         self.review_df.to_csv(str(file_name), encoding="cp932")
 
 
+    def print_json(self, json_data):
+        print(json.dumps(json_data, ensure_ascii=False, indent=4, separators=(',', ': ')))
 
 
+    def compare_list(self, list_a, list_b):
+        return list(set(list_a).symmetric_difference(set(list_b)))
+
+
+
+#rc = review_collecter_from_gurunavi()
+#rc.create_request_url('latitude=35.659272', 'longitude=139.697958','range=1')
+#rc.create_request_url('shop_id=g581402,g581405,a491700,a919400')
+#rc.create_request_url('latitude=35.607353', 'longitude=139.685616', 'range=1')
+#rc.send_request()
+#rc.write_to_csv('shop_review.csv')
+#print(rc.review_df.loc[:,'comment'])
